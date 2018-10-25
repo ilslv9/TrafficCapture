89ea7ee4df1ac5fd4e01f209937541217efa0348
kenny3run <kenny_3run@mail.ru>
2018-10-25 14:07:29 +0300

    base linux c sniffer

---
+++ TraficAnalyzer/cmake-build-debug/CMakeFiles/TraficAnalyzer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TraficAnalyzer.dir/main.cpp.o"
+  "TraficAnalyzer.pdb"
+  "TraficAnalyzer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TraficAnalyzer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
