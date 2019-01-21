7b85e4396f1cb49dc721a6b174ec8f2ed485de9e
kenny3run <kenny_3run@mail.ru>
2019-01-21 20:32:41 +0300

    Base impl

--- TraficAnalyzer/user_facade.h
+++ TraficAnalyzer/user_facade.h
@@ -3,6 +3,10 @@
 
 
 #include <pcap.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <iomanip>
+#include "../structures.h"
 
 class UserFacade {
 public:
@@ -33,12 +37,10 @@ public:
     }
 
     u_char getPacket() {
-        std::cout << "Wait packet..." << std::endl;
-        auto id = pcap_next(handle, &header);
-        std::cout << "Packet captured succesfully. "
-                  << "Packet length: " << header.len << " "
-                  << "Header length: " << header.caplen << std::endl;
-        return *id;
+        std::cout << "Wait packets..." << std::endl;
+//        auto id = pcap_next(handle, &header);
+
+        pcap_loop(handle, -1, captured_packet_callback, nullptr);
     }
 
     ~UserFacade() {
@@ -52,6 +54,154 @@ private:
     bpf_program fp;//Фильтр
     bpf_u_int32 net;//Ip устройства
     struct pcap_pkthdr header;//Заголовок PCAP
+    static void print_hex_ascii_line(const u_char *payload, int len, int offset) {
+
+        int i;
+        int gap;
+        const u_char *ch;
+
+        //print offset
+        std::cout << std::setw(5) << std::setfill('0') << offset << "   ";
+
+        //print hex
+        ch = payload;
+        for (i = 0; i < len; i++) {
+            std::cout << std::setw(2) << std::setfill('0') << std::hex << static_cast<int>(*ch) << " ";
+            ch++;
+            if (i == 7)
+                std::cout << " ";
+        }
+
+        if (len < 8)
+            std::cout << " ";
+
+        if (len < 16) {
+            gap = 16 - len;
+            for (i = 0; i < gap; i++) {
+                std::cout << "   ";
+            }
+        }
+        std::cout << "   ";
+
+        //ascii
+        ch = payload;
+        for (i = 0; i < len; i++) {
+            if (isprint(*ch))
+                std::cout << *ch;
+            else
+                std::cout << ".";
+            ch++;
+        }
+
+        std::cout << std::endl;
+    }
+
+    static void print_payload(const u_char *payload, int len) {
+
+        int len_rem = len;
+        int line_width = 16;//bytes number in line
+        int line_len;
+        int offset = 0;//offset counter
+        const u_char *ch = payload;
+
+        if (len <= 0)
+            return;
+
+        //if data in one line
+        if (len <= line_width) {
+            print_hex_ascii_line(ch, len, offset);
+            return;
+        }
+
+        //multiple lines
+        for (;;) {
+            //line length
+            line_len = line_width % len_rem;
+            //print line
+            print_hex_ascii_line(ch, line_len, offset);
+            //total remaining
+            len_rem = len_rem - line_len;
+            //shift pointer to remaining bytes to print
+            ch = ch + line_len;
+            //add offset
+            offset = offset + line_width;
+
+            if (len_rem <= line_width) {
+                //print last line
+                print_hex_ascii_line(ch, len_rem, offset);
+                break;
+            }
+        }
+    }
+
+    static void captured_packet_callback(u_char *args, const pcap_pkthdr *header, const u_char *packet) {
+        static int count = 1;//packet counter
+
+        //packet headers
+        const struct sniff_ip *ip;//ip header
+        const struct sniff_tcp *tcp;//tcp header
+        const char *payload;//payload
+
+        int size_ip;
+        int size_tcp;
+        int size_payload;
+
+        std::cout << std::endl << "Packet number " << count << ":" << std::endl;
+        count++;
+
+        //compute ip header offset
+        ip = (struct sniff_ip *) (packet + SIZE_ETHERNET);
+        size_ip = IP_HL(ip) * 4;
+        if (size_ip < 20) {
+            std::cout << "   Invalid ip header length: " << size_ip << " bytes" << std::endl;
+            return;
+        }
+
+        //Print from to ip addresses
+        std::cout << " From: " << inet_ntoa(ip->ip_src) << std::endl;
+        std::cout << " To: " << inet_ntoa(ip->ip_dst) << std::endl;
+        //Protocol
+        switch (ip->ip_p) {
+            case IPPROTO_TCP:
+                std::cout << " Protocol: TCP" << std::endl;
+                break;
+            case IPPROTO_UDP:
+                std::cout << " Protocol: UDP" << std::endl;
+                return;
+            case IPPROTO_ICMP:
+                std::cout << " Protocol: ICMP" << std::endl;
+                return;
+            case IPPROTO_IP:
+                std::cout << " Protocol: IP" << std::endl;
+                return;
+            default:
+                std::cout << " Protocol: unknown" << std::endl;
+                return;
+        }
+
+        //compute tcp header offset
+        tcp = (struct sniff_tcp *) (packet + SIZE_ETHERNET + size_ip);
+        size_tcp = TH_OFF(tcp) * 4;
+        if (size_tcp < 20) {
+            std::cout << "   Invalid TCP header length: " << size_tcp << " bytes" << std::endl;
+            return;
+        }
+
+        std::cout << " Src port: " << ntohl(tcp->th_sport) << std::endl;
+        std::cout << " Dst port: " << ntohl(tcp->th_dport) << std::endl;
+
+        //compute tcp payload offset
+        payload = reinterpret_cast<const char *>((u_char *) (packet + SIZE_ETHERNET + size_ip + size_tcp));
+
+        //compute tcp payload size
+        size_payload = ntohs(ip->ip_len) - (size_ip + size_tcp);
+
+        //print payload
+        if (size_payload > 0) {
+            std::cout << "   Payload (" << size_payload << " bytes):" << std::endl;
+            print_payload(reinterpret_cast<const u_char *>(payload), size_payload);
+        }
+    }
 };
 
 
